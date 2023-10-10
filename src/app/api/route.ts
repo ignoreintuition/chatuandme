92480e897b7085f399d0788918166309a90c834e
Brian Greig <brian@resurgencewebdesign.com>
2023-10-10 01:45:20 -0400

    Updated to use nextRequest

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,6 @@
+import { NextResponse } from "next/server";
 import { questions } from "./question";
 export async function GET(request: Request) {
   const randomSeed = Math.floor(Math.random() * questions.length);
-  return Response.json(questions[randomSeed]);
+  return NextResponse.json(questions[randomSeed]);
 }
